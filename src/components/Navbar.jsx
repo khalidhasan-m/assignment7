39111d246562dd796db59fece2a5e8758c743c7e
Khalid Hasan <khalidhasanmeskat@gmail.com>
2026-05-10 02:20:38 +0600

    make the navbar sticky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,27 +5,32 @@ import { Link } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm flex justify-between px-3">
+    <div className="navbar bg-base-100 shadow-sm flex justify-between px-3 sticky top-0 z-50">
+      
       <div>
         <Link to="/" className="btn btn-ghost text-xl">
           KeenKeeper
         </Link>
       </div>
+
       <div>
         <ul>
           <li className="flex gap-4 justify-around px-3">
             <Link className="btn btn-ghost" to="/">
               <IoMdHome /> Home
             </Link>
+
             <Link className="btn btn-ghost" to="/timeline">
               <IoTimeOutline /> Timeline
             </Link>
+
             <Link className="btn btn-ghost" to="/stats">
               <ImStatsDots /> Stats
             </Link>
           </li>
         </ul>
       </div>
+
     </div>
   );
 };
